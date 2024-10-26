ead402a9cca2ffa5c045a70935899f7f54790387
WebDev-Projects <sirimallasanjana523@gmail.com>
2024-10-26 19:48:30 +0530

    oct 26

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -10,21 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-=======
-import "dotenv/config";
-import { app } from "./app.js";
-import connectDB from "./db/connectDB.js";
-
-connectDB()
-  .then(() => {
-    app.on("error", (err) => {
-      console.log("Application Error", err);
-    });
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on PORT : ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("DB connection error", err);
-  });
->>>>>>> 808da5e0d378a9b890d312b4ea51d18003c3d329
